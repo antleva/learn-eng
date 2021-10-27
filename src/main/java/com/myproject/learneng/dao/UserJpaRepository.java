91295b4c0ba73937ea01ebbbaf4b3529c180ca0f
antleva <anton.levanov.89@mail.ru>
2021-10-27 20:59:46 +0300

    change app.component.html

--- src/main/java/com/myproject/learneng/dao/UserJpaRepository.java
+++ src/main/java/com/myproject/learneng/dao/UserJpaRepository.java
@@ -9,6 +9,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserJpaRepository extends JpaRepository<User,Long> {
+
     Optional<User> findByName(String name);
 
     @Query(value = "select distinct u from User u join fetch u.setResults sr")
