2a86136878d571314cf0e48c8156422283b8ef63
antleva <anton.levanov.89@mail.ru>
2021-11-12 21:24:35 +0300

    add concurrent executor in PhrasePopulator

--- src/main/java/com/myproject/learneng/util/PhrasesPopulator.java
+++ src/main/java/com/myproject/learneng/util/PhrasesPopulator.java
@@ -8,15 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
-@EnableAsync
 @Component
 public class PhrasesPopulator implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -24,30 +25,45 @@ public class PhrasesPopulator implements ApplicationListener<ContextRefreshedEve
     private VideoRepository videoRepository;
 
     @Autowired
-    public PhrasesPopulator(PhraseService phraseService, VideoRepository videoRepository ) {
+    public PhrasesPopulator(PhraseService phraseService, VideoRepository videoRepository) {
         this.phraseService = phraseService;
         this.videoRepository = videoRepository;
     }
 
-    @Async
     public void populate() throws IOException {
+        ExecutorService executor = Executors.newCachedThreadPool();
+        List<Callable<String>> callables = new ArrayList<>();
         InputStream inputStream = new ClassPathResource("phrases.txt").getInputStream();
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))){
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
             String s;
-            while ((s = bufferedReader.readLine())!=null){
-                String [] strings = s.split(" : ");
-                parseAndSavePhrases(strings);
+            while ((s = bufferedReader.readLine()) != null) {
+                String[] strings = s.split(" : ");
+                callables.add(() -> parseAndSavePhrases(strings));
             }
+
+            List<Future<String>> futures = executor.invokeAll(callables);
+            for (Future<String> f : futures) {
+                f.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
         }
+
     }
 
-    public void parseAndSavePhrases(String[] strings) throws IOException {
+
+    public String parseAndSavePhrases(String[] strings) {
         String description = strings[0];
         String translation = strings[1];
         PhraseType phraseType = PhraseType.valueOf(strings[2]);
-        Phrase phrase = new Phrase(phraseType,description,translation);
+        Phrase phrase = new Phrase(phraseType, description, translation);
         phraseService.create(phrase);
-        videoRepository.addVideo(phrase.getDescription(), VideoLoader.parseVideo(phrase.getDescription(), 1));
+        try {
+            videoRepository.addVideo(phrase.getDescription(), VideoLoader.parseVideo(phrase.getDescription(), 1));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "phrase is added";
     }
 
 
